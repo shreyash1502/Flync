50840125a9c049b34135cc688bdbbb3489ab2e1d
Aryan <aryan.arora180@gmail.com>
2019-05-23 12:07:26 +0530

    Added ability to see total and free space

--- app/src/main/java/com/benrostudios/flync/HomeFragment.java
+++ app/src/main/java/com/benrostudios/flync/HomeFragment.java
@@ -3,19 +3,36 @@ package com.benrostudios.flync;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StatFs;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.io.File;
+
 public class HomeFragment extends Fragment {
+
+    private final static double noBytesInOneGB = 1073741824.0;
+
+    private TextView storageInfoTextView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View fragview = inflater.inflate(R.layout.fragment_home, null);
+        storageInfoTextView = fragview.findViewById(R.id.storageTextView);
 
+        //code to find out how much storage exists and how much is used
+        StatFs stat = new StatFs(Environment.getDataDirectory().getAbsolutePath());
+        double totalSpaceInGB = stat.getTotalBytes()/noBytesInOneGB;
+        double freeSpaceInGB = stat.getAvailableBytes()/noBytesInOneGB;
+        storageInfoTextView.setText("total: " + totalSpaceInGB + "\n free: " + freeSpaceInGB);
 
         return fragview;
     }
