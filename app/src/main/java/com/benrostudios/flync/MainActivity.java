d64d45fcbaa486d11cb959addd53827dac42cb2e
Hemanth Krishna <31009634+darthbenro008@users.noreply.github.com>
2019-05-27 18:29:43 +0530

    File Picker + File Sending has been linked addtionally settling fragment dispute

--- app/src/main/java/com/benrostudios/flync/MainActivity.java
+++ app/src/main/java/com/benrostudios/flync/MainActivity.java
@@ -1,17 +1,22 @@
 package com.benrostudios.flync;
 
+
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
-import android.widget.TextView;
+
+import android.widget.Toast;
 
 public class
 MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 //You always Implement the BottomNavBar when working with frags
 
+
+public static String tag;
+public static Fragment passer;
 
 
     @Override
@@ -19,21 +24,29 @@ MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigat
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
        //Just defining where the Nav Bar actually is
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.nav_view);
         navigation.setSelectedItemId(R.id.navigation_home);
         navigation.setOnNavigationItemSelectedListener(this);
         //Loading the default Fragment that should be loaded at start of the app
-        loadFrag(new HomeFragment());
+        if(savedInstanceState != null){
+            tag = savedInstanceState.getString("FragTag");
+            getSupportFragmentManager().findFragmentByTag(tag);
+            Toast.makeText(this, "FragReused", Toast.LENGTH_SHORT).show();
+
+        }else {
+            loadFrag(new HomeFragment() , "home");
+        }
     }
 //This is a fragment transaction , basically it manages the fragments and swaps them on the activity
-    private boolean loadFrag (Fragment fragment){
+    private boolean loadFrag (Fragment fragment, String Tag){
         if(fragment != null){
             getSupportFragmentManager()
                     .beginTransaction()
-                    .replace(R.id.frame,fragment)
+                    .replace(R.id.frame,fragment,Tag)
                     .commit();
-
+            Toast.makeText(this, tag, Toast.LENGTH_SHORT).show();
 
             return false;
         }
@@ -44,17 +57,35 @@ MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigat
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         Fragment fragmenthere = null;
         //A super simple switch case , ALL HAIL NETBEANS
+
         switch (menuItem.getItemId()){
-            case R.id.navigation_home : fragmenthere =  new HomeFragment();
+            case R.id.navigation_home : fragmenthere =  new HomeFragment();tag="home";
                 break;
-            case R.id.navigation_history : fragmenthere = new History();
+            case R.id.navigation_history : fragmenthere = new History();tag = "history";
                 break;
-            case R.id.navigation_settings : fragmenthere = new Settings();
+            case R.id.navigation_settings : fragmenthere = new Settings();tag = "settings";
                 break;}
 
-        loadFrag(fragmenthere);
+        loadFrag(fragmenthere,tag);
+        passer = fragmenthere;
 
 
         return true;
     }
+
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("FragTag",tag);
+
+    }
+    public static Fragment getFrag(){
+        return passer;
+
+    }
+
+
+
 }
--- app/src/main/java/com/benrostudios/flync/Settings.java
+++ app/src/main/java/com/benrostudios/flync/Settings.java
@@ -1,40 +1,121 @@
 package com.benrostudios.flync;
 
-import android.content.Context;
+import android.Manifest;
 import android.content.Intent;
-import android.database.Cursor;
-import android.net.Uri;
+import android.content.pm.PackageManager;
+
 import android.os.Bundle;
-import android.provider.MediaStore;
+
+import android.os.StrictMode;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.button.MaterialButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
 import android.widget.Toast;
+import com.nbsp.materialfilepicker.MaterialFilePicker;
+import com.nbsp.materialfilepicker.ui.FilePickerActivity;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 
 
 public class Settings extends Fragment {
+
+
+
+
+
+
+    private String selectedFilePath;
+
+    public static final int PERMISSIONS_REQUEST_CODE = 0;
+    public static final int FILE_PICKER_REQUEST_CODE = 1;
     @Nullable
+
+
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if(getActivity()==null)
+            Toast.makeText(null, "Activity is null", Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View fragview = inflater.inflate(R.layout.fragment_settings, null);
+        if(getActivity()==null)
+            Toast.makeText(null, "NullActivity", Toast.LENGTH_SHORT).show();
         //Just Adding the Button Listener here
         Button Mab = fragview.findViewById(R.id.explorer);
+        StrictMode.ThreadPolicy policy =
+                new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         Mab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Creating a new Intent to invoke the File Manager
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.setType("image/*");
-                startActivityForResult(intent, 100);
+                checkPermissionsAndOpenFilePicker();
             }
         });
+        Button Sab = fragview.findViewById(R.id.sned);
+        Sab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Socket sock;
+                try {
+
+                    sock = new Socket("192.168.0.109", 1149);
+                    System.out.println("Connecting...");
+
+                    // sendfile
+
+                    File myFile = new File(selectedFilePath);
+
+                    byte [] mybytearray  = new byte [(int)myFile.length()];
+                    Toast.makeText(getActivity(), mybytearray.toString(), Toast.LENGTH_SHORT).show();
+
+
+
+                    FileInputStream fis = new FileInputStream(myFile);
+                    BufferedInputStream bis = new BufferedInputStream(fis);
+                    bis.read(mybytearray,0,mybytearray.length);
+                    OutputStream os = sock.getOutputStream();
+                    Toast.makeText(getActivity(), "Sending.....", Toast.LENGTH_SHORT).show();
+                    os.write(mybytearray,0,mybytearray.length);
+                    os.flush();
+
+                    sock.close();
+                    Toast.makeText(getActivity(), "Sent!", Toast.LENGTH_SHORT).show();
+                } catch (UnknownHostException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+
+
 
         return fragview;
     }
@@ -44,17 +125,66 @@ public class Settings extends Fragment {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         // The Request code was randomly assigned , if you are wondering
-        if (requestCode == 100) {
-            //Dont Mind these debug toasts
-            Toast.makeText(getActivity(), "ReqCodeMila", Toast.LENGTH_SHORT).show();
-             // we get the uri of selected file here
-            Uri uri = data.getData();
-            if (uri != null) {
-                String filename;
-                 filename = uri.getPath();
-                 //We Toast the URI Path
-                Toast.makeText(getActivity(), filename, Toast.LENGTH_SHORT).show();
+        if (requestCode == FILE_PICKER_REQUEST_CODE ) {
+            String path = data.getStringExtra(FilePickerActivity.RESULT_FILE_PATH);
+
+            selectedFilePath = path;
+
+            if (path != null) {
+                Log.d("Path: ", path);
+                Toast.makeText(getContext(), "Picked file: " + path, Toast.LENGTH_LONG).show();
+
             }
         }
     }
+
+    private void checkPermissionsAndOpenFilePicker() {
+        String permission = Manifest.permission.READ_EXTERNAL_STORAGE;
+
+        if (ContextCompat.checkSelfPermission(getActivity(), permission) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), permission)) {
+                showError();
+            } else {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{permission}, PERMISSIONS_REQUEST_CODE);
+            }
+        } else {
+            openFilePicker();
+        }
+    }
+
+    private void showError() {
+        Toast.makeText(getActivity(), "Allow external storage reading", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_CODE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openFilePicker();
+                } else {
+                    showError();
+                }
+            }
+        }
+    }
+
+    public void openFilePicker() {
+
+
+        new MaterialFilePicker()
+
+                .withRequestCode(1)
+                .withSupportFragment(MainActivity.getFrag())
+                .withFilterDirectories(true) // Set directories filterable (false by default)
+                .withHiddenFiles(false) // Show hidden files and folders
+                .start();
+        
+    }
+
+
+
+
 }
