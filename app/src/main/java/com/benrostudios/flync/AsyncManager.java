0f2ac4a5d50f9da6bf035ec32a7968dd2dbf12cc
Hemanth Krishna <31009634+darthbenro008@users.noreply.github.com>
2019-05-31 23:35:56 +0530

    Switching back to default file manager selection, added multiple files selection

--- app/src/main/java/com/benrostudios/flync/AsyncManager.java
+++ app/src/main/java/com/benrostudios/flync/AsyncManager.java
@@ -1,7 +1,9 @@
 package com.benrostudios.flync;
 import android.app.Activity;
 import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedInputStream;
 import java.io.DataOutputStream;
@@ -9,73 +11,76 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.lang.reflect.Array;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 
-public class AsyncManager extends AsyncTask<ArrayList<String>, Integer ,String> {
+public class AsyncManager extends AsyncTask<String, Integer ,String> {
 
     Activity mContext;
     Integer mode;
-    TextView le ;
+    private ArrayList<String> fileNameAndPaths ;
+    String[] filenames ;
 
 
 
-    public  AsyncManager(Activity context,Integer mode)
+    public  AsyncManager(Activity context,Integer mode,ArrayList<String> fnp)
     {
         this.mContext=context;
         this.mode = mode;
-        le =  (TextView) mContext.findViewById(R.id.le);
+        this.fileNameAndPaths= fnp;
+
 
 
     }
     @Override
-    protected String doInBackground(ArrayList<String>... params) {
-        String selectedFilePath = params[0].toString();
-        String filename = params[1].toString();
-
-        if(mode==1) {
-
-            Socket sock;
-            try {
+    protected String doInBackground(String... params) {
+        for(int i=0,j=1; i <= fileNameAndPaths.size()-1;i=i+2,j=i+1) {
+            String selectedFilePath = fileNameAndPaths.get(j);
+            String filename = fileNameAndPaths.get(i);
 
 
-                // sendfile
-                sock = new Socket("192.168.0.109", 1149);
-                File myFile = new File(selectedFilePath);
-                long filesize = myFile.length();
-                DataOutputStream DOS = new DataOutputStream(sock.getOutputStream());
-                DOS.writeUTF(filename + "/" + filesize);
 
-                byte[] mybytearray = new byte[(int) myFile.length()];
+            if (mode == 1) {
+
+                Socket sock;
+                try {
 
 
-                FileInputStream fis = new FileInputStream(myFile);
-                BufferedInputStream bis = new BufferedInputStream(fis);
-                bis.read(mybytearray, 0, mybytearray.length);
-                OutputStream os = sock.getOutputStream();
+                    // sendfile
+                    sock = new Socket("192.168.0.109", 1149);
+                    File myFile = new File(selectedFilePath);
+                    long filesize = myFile.length();
+                    Log.d("sentfiles",selectedFilePath+" "+filesize);
+                    DataOutputStream DOS = new DataOutputStream(sock.getOutputStream());
+                    DOS.writeUTF(filename + "/" + filesize);
 
-                os.write(mybytearray, 0, mybytearray.length);
-                os.flush();
+                    byte[] mybytearray = new byte[(int) myFile.length()];
 
-                sock.close();
 
-            } catch (UnknownHostException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+                    FileInputStream fis = new FileInputStream(myFile);
+                    BufferedInputStream bis = new BufferedInputStream(fis);
+                    bis.read(mybytearray, 0, mybytearray.length);
+                    OutputStream os = sock.getOutputStream();
+
+                    os.write(mybytearray, 0, mybytearray.length);
+                    os.flush();
+
+                    sock.close();
+
+                } catch (UnknownHostException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            } else if (mode == 2) {
+                //Receive Async Task here
+
+
             }
-        }else if(mode == 2){
-            //Recive Async Task here
-
-
-
-
-
-
-
         }
 
         return null;
@@ -83,13 +88,14 @@ public class AsyncManager extends AsyncTask<ArrayList<String>, Integer ,String> 
 
     @Override
     protected void onProgressUpdate(Integer... progress) {
-        le.setText("Progress:" + progress[0]);
+
 
 
     }
 
     @Override
     protected void onPostExecute(String bitmaps) {
+        Toast.makeText(mContext, "Sent!", Toast.LENGTH_SHORT).show();
 
     }
 }
--- app/src/main/java/com/benrostudios/flync/Settings.java
+++ app/src/main/java/com/benrostudios/flync/Settings.java
@@ -1,21 +1,28 @@
 package com.benrostudios.flync;
 
 import android.Manifest;
-import android.app.Activity;
+import android.content.ClipData;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 
 
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 
+import android.os.Environment;
 import android.os.StrictMode;
 
+import android.provider.MediaStore;
+
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -24,46 +31,43 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
-import com.nbsp.materialfilepicker.MaterialFilePicker;
-import com.nbsp.materialfilepicker.ui.FilePickerActivity;
 
+
+import java.io.File;
 import java.util.ArrayList;
-
-import droidninja.filepicker.FilePickerBuilder;
-import droidninja.filepicker.FilePickerConst;
+import java.util.Arrays;
 
 
 public class Settings extends Fragment {
 
 
-
-
-
-
     private String selectedFilePath;
+    private Uri selectedFileURI;
+  
+    private ArrayList<String> fileNameAndPaths = new ArrayList<>();
     private String filename;
-    private ArrayList<String> filePaths = new ArrayList<>();
-    private ArrayList<String> photoPaths = new ArrayList<>();
+    private String pathtofile;
     private int filesize;
     public static final int PERMISSIONS_REQUEST_CODE = 0;
     public static final int FILE_PICKER_REQUEST_CODE = 1;
+    private String action = "android.intent.action.MULTIPLE_PICK";
+
     @Nullable
-
-
 
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        if(getActivity()==null)
+        if (getActivity() == null)
             Toast.makeText(null, "Activity is null", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View fragview = inflater.inflate(R.layout.fragment_settings, null);
-        if(getActivity()==null)
+        if (getActivity() == null)
             Toast.makeText(null, "NullActivity", Toast.LENGTH_SHORT).show();
+       
         //Just Adding the Button Listener here
         Button Mab = fragview.findViewById(R.id.explorer);
         StrictMode.ThreadPolicy policy =
@@ -82,12 +86,9 @@ public class Settings extends Fragment {
             @Override
             public void onClick(View view) {
 
-            new AsyncManager(getActivity(),1).execute(filePaths);
+                new AsyncManager(getActivity(), 1,fileNameAndPaths).execute("");
             }
         });
-
-
-
 
 
         return fragview;
@@ -98,24 +99,75 @@ public class Settings extends Fragment {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         // The Request code was randomly assigned , if you are wondering
-        switch (requestCode)
-        {
-            case FilePickerConst.REQUEST_CODE_PHOTO:
-                if(resultCode== Activity.RESULT_OK && data!=null)
-                {
-                    photoPaths = new ArrayList<>();
-                    photoPaths.addAll(data.getStringArrayListExtra(FilePickerConst.KEY_SELECTED_MEDIA));
+        if (data != null) {
+            if (requestCode == FILE_PICKER_REQUEST_CODE) {
+
+
+                ClipData clipData = data.getClipData();
+                if (clipData != null) {
+                      fileNameAndPaths.clear();
+                    for (int i = 0; i < data.getClipData().getItemCount(); i++) {
+                        Uri uri = data.getClipData().getItemAt(i).getUri();
+                        File file = new File(uri.getPath());//create path from uri
+                        Log.d("ExMania", file.getPath());
+                        ArrayList<String> aList = new ArrayList<String>(Arrays.asList(file.getPath().split(":")));
+                        Log.d("ExMania", aList.toString());
+                        try{
+                            pathtofile = aList.get(1);
+                            filename = nameSplitter(pathtofile,2);
+
+
+                        }catch(Exception e){
+
+                            pathtofile = getRealPathFromURI(uri);
+                            filename = nameSplitter(pathtofile,2);
+
+                        }
+
+
+
+                        Log.d("ExMania", pathtofile);
+                        fileNameAndPaths.add(filename);
+                        fileNameAndPaths.add(pathtofile);
+                        aList.clear();
+
+                        selectedFilePath = pathtofile;
+
+
+                    }
+
+
+                } else {
+
+                    selectedFileURI = data.getData();
+                    File file = new File(selectedFileURI.getPath());//create path from uri
+                    Log.d("ExMania", selectedFileURI.toString());
+                    final String[] split = file.getPath().split(":");//split the path.
+                    try{
+                        selectedFilePath = split[1];
+                        filename = nameSplitter(selectedFilePath,1);
+                    }catch(Exception e){
+                        selectedFilePath = getRealPathFromURI(selectedFileURI);
+                        filename = nameSplitter(selectedFilePath,1);
+
+                    }
+                    fileNameAndPaths.add(filename);
+                    fileNameAndPaths.add(selectedFilePath);
+
                 }
-                break;
-            case FilePickerConst.REQUEST_CODE_DOC:
-                if(resultCode== Activity.RESULT_OK && data!=null)
-                {
-                    filePaths = new ArrayList<>();
-                    filePaths.addAll(data.getStringArrayListExtra(FilePickerConst.KEY_SELECTED_DOCS));
-                }
-                break;
+
+                TextView len = getView().findViewById(R.id.explorer);
+
+
+                len.setText("" + selectedFilePath);
+
+                Log.d("ExMania", fileNameAndPaths.toString());
+
+
+            }
         }
     }
+
 
     private void checkPermissionsAndOpenFilePicker() {
         String permission = Manifest.permission.READ_EXTERNAL_STORAGE;
@@ -151,19 +203,50 @@ public class Settings extends Fragment {
     }
 
     public void openFilePicker() {
-
-
-        FilePickerBuilder.getInstance().setMaxCount(5)
-                .setSelectedFiles(filePaths)
-                .setActivityTheme(R.style.LibAppTheme)
-                .pickFile(this, FILE_PICKER_REQUEST_CODE);
+        Intent chooseFile;
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        chooseFile = Intent.createChooser(intent, "Choose a file");
+        startActivityForResult(chooseFile, FILE_PICKER_REQUEST_CODE);
 
     }
 
+    private String getRealPathFromURI(Uri uri) {
+        Cursor cursor = null;
+        String path = null;
+        try {
+            cursor = getActivity().getContentResolver().query(uri, new String[]{MediaStore.MediaColumns.DISPLAY_NAME}, null, null, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                String fileName = cursor.getString(0);
+                path = Environment.getExternalStorageDirectory().toString() + "/Download/" + fileName;
+                if (!TextUtils.isEmpty(path)) {
+                    return path;
+                }
+            }
+        } finally {
+            if (cursor != null) cursor.close();
+        }
+        return path;
+    }
+
+    private String nameSplitter(String name, int mode ){
+        String returnName;
+        if(mode == 1) {
+            String[] splitter = name.split("/");
+            int filepos = splitter.length - 1;
+            returnName = splitter[filepos];
+        }else{
+            ArrayList<String> b = new ArrayList<String>(Arrays.asList(name.split("/")));
+            int filepos = b.size()-1;
+            returnName = b.get(filepos);
+            b.clear();
 
 
 
+        }
 
 
-
+        return returnName;
+    }
 }
